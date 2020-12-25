1083da41c6739568f2dc98c89968e73aec20a919
DominicKarma <51094361+DominicKarma@users.noreply.github.com>
2020-12-24 20:19:19 -0600

    Removed the infinite inventory platinum

--- CalTestHelpersPlayer.cs
+++ CalTestHelpersPlayer.cs
@@ -6,17 +6,6 @@ namespace CalTestHelpers
 {
 	public class CalTestHelpersPlayer : ModPlayer
 	{
-		// Infinite money at all times.
-		public override void PostUpdate()
-		{
-			if (player.inventory[50].type != ItemID.PlatinumCoin ||
-				player.inventory[50].stack != 999)
-			{
-				player.inventory[50].SetDefaults(ItemID.PlatinumCoin);
-				player.inventory[50].stack = 999;
-			}
-		}
-
 		public override void ProcessTriggers(TriggersSet triggersSet)
 		{
 			if (CalTestHelpers.ToggleUIsHotkey.JustPressed)
